70735b28ff53fb1efc459057ef11b5d11833d012
RealAtix <lana20razvan@gmail.com>
2016-06-19 18:22:22 +0200

    Added tests to the package

--- package.js
+++ package.js
@@ -20,3 +20,10 @@ Package.onUse(function (api, where) {
   if (api.export) 
     api.export('Errors');
 });
+
+Package.onTest(function(api) {
+  api.use('atix:errors', 'client');
+  api.use(['templating', 'tinytest', 'test-helpers'], 'client');  
+
+  api.addFiles('errors_tests.js', 'client');
+});
