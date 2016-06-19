04a733baf7918e8d9a35da9984ff7bac5f9e0288
RealAtix <lana20razvan@gmail.com>
2016-06-19 18:16:21 +0200

    Added templates, helpers & collection

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # meteor-errors
 
 A pattern to display application errors to the user. It uses a local collection to collect the errors. Errors will fade out after displaying.
-Bootstrap is required.
+
+*Testing meteor packages (thanks to discover meteor)*
 
 To start:
 * Meteor: `meteor add atix:errors`
@@ -12,4 +13,3 @@ Add the template to your layout:
 
 Throw errors with:
 `Errors.throw(error);`
-
---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+  name: 'atix:errors',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'A pattern to display application errors to the user',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/RealAtix/meteor-errors.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function (api, where) {
+  api.versionsFrom('0.9.0');
+
+  api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
+
+  api.addFiles(['errors.js', 'errors_list.html', 'errors_list.js'], 'client');
+
+  if (api.export) 
+    api.export('Errors');
+});
